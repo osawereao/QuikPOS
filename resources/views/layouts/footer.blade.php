edb1d449c9f41dbf8323284fba3d249d6c725bf3
AO™ <anthony@osawere.com>
2024-04-27 12:34:49 +0100

    o

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="c-footer">
-    <div>QuikPOS © {{ date('Y') }} | <strong><a target="_blank" href="https://repo.ng">REPO.NG</a></strong></div>
+    <div>QuikPOS © {{ date('Y') }} • <strong><a target="_blank" href="https://repo.ng">REPO.NG</a></strong></div>
     <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
 </footer>
